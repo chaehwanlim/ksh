d1b1d9234c9951636c3dd3dd641fe3e84e86e08f
harry <chlim428@gmail.com>
2020-12-30 17:33:43 +0900

    Initial setting for sqlite3

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
 import * as express from "express";
 
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 5000;
+
 const app: express.Application = express();
 
 app.get(
@@ -8,5 +11,9 @@ app.get(
     
   }
 );
+
+app.listen(5000, () => {
+  console.log(`Server is listening on port ${port}`);
+});
 
 export default app;
--- sqlite3.ts
+++ sqlite3.ts
@@ -1,5 +1,11 @@
-
-
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 
-const dbPath = path.join(__dirname, "kshdb.db");
+const dbPath = path.join(__dirname, "db", "kshdb.db");
+const db = new sqlite3.Database(dbPath, (err: any) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Successful connection to the database 'apptest.db'");
+});
+
